034cc0f0ddfce1ac3dcb3d1dfe7c3f64327e13c0
dagkand <dagkand@ntnu.no>
2023-09-12 14:39:58 +0200

    temp

---
+++ core/src/main/java/core/WordGuesser.java
@@ -0,0 +1,46 @@
+package core;
+
+public class WordGuesser {
+    
+    private boolean isSubstring = false;
+    private String word;
+    private String substring;
+    private int score = 0;
+    private int life = 3;
+    private int timer = 30;
+
+
+
+
+
+    public void isSubstring(String word, String substring) {
+        boolean result = word.contains(substring);
+        
+        // Set the global variable isSubstring based on the result
+        this.isSubstring = result;
+    }
+
+    public void guess(String word, String substring){
+        if (isSubstring){
+            nextWord();
+            this.score++;
+        }
+
+        else{
+            this.timer-=5;
+
+        }
+
+    }
+
+    public void nextWord() {
+        
+    }
+
+    public void loseLife() {
+        if (this.timer==0){
+            this.life--;
+        }
+    }
+
+}
