3a6f0fb2f41c1a96f267433d3b9932f2d60600d4
andervs <andervs@stud.ntnu.no>
2023-09-12 22:30:51 +0200

    initiated WordMaster.java

---
+++ core/src/main/java/core/WordMaster.java
@@ -0,0 +1,41 @@
+package core;
+
+public class WordMaster 
+{
+
+    /**
+     * Provides a reference to the selected category
+     * @return A randomly generated substring from the parameter
+     */
+    public String getCategory()
+    {
+        //TODO
+        //could return instance of "Category" class instead of string
+        //Could possibly create a "Category" class, since each user has their own set of categories
+        //Could have an implemented "Category" class, and a "CustomCategory" class which inherits from "Category",
+        //since a user can expand the basic categories with their own categories
+        return null;
+    }
+
+    /**
+     * Chooses a word randomly from the selected category
+     * @return A randomly generated substring from the parameter
+     */
+    public String  getRandomWord()
+    {
+        //TODO
+        return null;
+    }
+
+    /**
+     * Randomly generates a substring from the randomly chosen word
+     * @param word The chosen word to generate a substring from
+     * @return A randomly generated substring from the parameter
+     */
+    public static String getRandomSubstring(String word)
+    {
+        //TODO
+        return null;
+    }
+    
+}
