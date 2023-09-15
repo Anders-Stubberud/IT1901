40937b2537b3c5e3c8cbb9bcf4a91f5215b5f407
andervs <andervs@stud.ntnu.no>
2023-09-15 07:39:50 +0200

    Started implementation of CategorySetup tests

--- core/src/test/java/core/CategorySetupTest.java
+++ core/src/test/java/core/CategorySetupTest.java
@@ -1,5 +1,27 @@
 package core;
 
-public class CategorySetupTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategorySetupTest 
+{
     
+    /**
+     * Sets up two instances of the CategorySetup class to be used in the tests.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        CategorySetup guestUser = new CategorySetup(null);
+        CategorySetup registeredUser = new CategorySetup("example_user");
+    }
+
+    // @Test
+    // @DisplayName
+    // public void testAquireDefaultCategories()
+    // {
+        
+    // }
+
 }
