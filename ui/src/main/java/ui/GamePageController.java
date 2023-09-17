156749e8f4431c7a248a833d87cfeae0b84ed58b
madsab <mads.barnes@hotmail.com>
2023-09-17 21:01:03 +0200

    added createPlayers, checkWrittenWord, rndWord

--- ui/src/main/java/ui/GamePageController.java
+++ ui/src/main/java/ui/GamePageController.java
@@ -3,14 +3,23 @@ package ui;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
@@ -22,7 +31,14 @@ public class GamePageController implements Initializable {
     @FXML
     private Pane WordMaster;
     @FXML
+    private AnchorPane window;
+    @FXML
     private TextField PlayerInputField;
+    @FXML
+    private Label letters, points;
+
+    private List<Circle> players = new ArrayList<>();
+    private List<String> categoryAnswers = new ArrayList<>(Arrays.asList("norway"));
 
     /**
      * Move the WordMaster (The letters) to a chosen location. Resets to original
@@ -49,18 +65,79 @@ public class GamePageController implements Initializable {
 
     }
 
-    // Write a word, displays it on screen TODO
-    public void writeWord() {
-        WordMasterMoveTo(profileCircle2.getLayoutX(), profileCircle2.getLayoutY(), 1000);
-        PlayerInputField.requestFocus();
+    /**
+     * Create players and draw them on the fxml
+     * 
+     * @param haveBots - If true add 1-4 bots
+     * 
+     * @throws FileNotFoundException If player pictures is not found
+     */
+    public void createPlayers(boolean haveBots) throws FileNotFoundException {
+        // TODO read from active player JSON
+        int radius = 30;
+        int centerY = 425; // Default Y position
+        double playerCenterX = 500; // Default X is center
+
+        if (haveBots) {
+            double NumOfBots = Math.floor(Math.random() * 5) + 2;// minimum of 1 bot
+            double centerX = window.getPrefWidth() / (NumOfBots + 1);
+            for (int j = 1; j < (NumOfBots + 1); j++) {
+                if (j != (int) Math.ceil(NumOfBots / 2)) {
+                    players.add(new Circle(centerX * j, centerY, radius, Color.BLACK));
+                } else {
+                    playerCenterX = centerX * j;
+                }
+            }
+        }
+        Circle activePlayer = new Circle(playerCenterX, centerY, radius,
+                new ImagePattern(new Image(new FileInputStream("./assets/images/Brage.png"))));
+
+        players.add(((int) Math.floor(players.size() / 2)), activePlayer);
+        window.getChildren().addAll(players);
+
+    }
+
+    /**
+     * WordMaster checks if player written word is correct. If right add 1 point
+     */
+    public void checkWrittenWord(KeyEvent ke) {
+        // TODO green color on right letters and red on wrong
+
+        if (ke.getCode().equals(KeyCode.ENTER)) { // If pressed Enter, then check word
+            String playerGuess = PlayerInputField.getText().toLowerCase();
+            if (playerGuess.contains(letters.getText().toLowerCase())
+                    && categoryAnswers.contains(playerGuess.toLowerCase())) {
+
+                // TODO - FileWriter add points
+                int Points = Integer.parseInt(points.getText()) + 1;
+                points.setText(String.valueOf(Points));
+                rndWordMasterLetters();
+            } else {
+                // TODO - Animate shake effect on wrong answer
+            }
+
+        }
+
+    }
+
+    /**
+     * Displays a random set of letters from the category answers.
+     * The length of the letters is either 2 or 3.
+     */
+    public void rndWordMasterLetters() {
+        String rndCategoryWord = categoryAnswers.get(new Random().nextInt(categoryAnswers.size()));
+        int rndIndex = (int) Math.floor(Math.random() * (rndCategoryWord.length() - 3));
+        int strLength = (int) Math.floor(Math.random() * 2) + 2;
+        String finalString = rndCategoryWord.substring(rndIndex, rndIndex + strLength);
+        letters.setText(finalString.toUpperCase());
     }
 
     @Override // Runs on start of the application
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            Image profileImg = new Image(new FileInputStream("./assets/images/Brage.png"));
-
-            profileCircle.setFill(new ImagePattern(profileImg));
+            rndWordMasterLetters();
+            createPlayers(true);
+            PlayerInputField.requestFocus();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
