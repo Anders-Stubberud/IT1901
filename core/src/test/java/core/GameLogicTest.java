e77299f761e3728231fef7745a08edcc3d006377
andervs <andervs@stud.ntnu.no>
2023-09-19 12:25:29 +0200

    Rename to GameLogicTest.java

--- core/src/test/java/core/GameLogicTest.java
+++ core/src/test/java/core/GameLogicTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CategoryTest 
+public class GameLogicTest 
 {
 
     private GameLogic guest;
