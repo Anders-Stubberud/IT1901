94744c7ffb49a30ae579ed6343afae64b028f4e6
dagkand <dagkand@ntnu.no>
2023-09-20 15:49:17 +0200

    fix some

--- ui/src/main/java/ui/GamePageController.java
+++ ui/src/main/java/ui/GamePageController.java
@@ -4,9 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 import core.FileIO;
@@ -28,64 +26,115 @@ import javafx.util.Duration;
 
 public class GamePageController implements Initializable {
 
+    /**
+     * The profileCircle is the circle that represents the player.
+     */
     @FXML
     private Circle profileCircle, profileCircle1, profileCircle2, profileCircle3;
+    /**
+     * The wordMaster is the pane that contains the letters.
+     */
     @FXML
-    private Pane WordMaster;
+    private Pane wordMaster;
+    /**
+     * The window is the pane that contains the game.
+     */
     @FXML
     private AnchorPane window;
+    /**
+     * The playerInputField is the textfield where the player writes the word.
+     */
     @FXML
-    private TextField PlayerInputField;
+    private TextField playerInputField;
+    /**
+     * The letters is the label that contains the letters.
+     */
     @FXML
     private Label letters, points;
 
+    /**
+     * The user is the GameLogic object that is used to get the words.
+     */
     private GameLogic user;
+    /**
+     * The substring is the letters that the player has to use.
+     */
     private String substring;
+    /**
+     * The players is the list of players.
+     */
     private List<Circle> players = new ArrayList<>();
+    /**
+     * The layoutX is the X position of the wordMaster.
+     */
+    private final int layoutX = 470;
+    /**
+     * The layoutY is the Y position of the wordMaster.
+     */
+    private final int layoutY = 30;
+    /**
+     * The layoutCenter is the center of the wordMaster.
+     */
+    private final int layoutCenter = 30;
 
     /**
-     * Move the WordMaster (The letters) to a chosen location. Resets to original
+     * The radius is the radius of the player circle.
+     */
+    private final int radius = 30;
+
+    /**
+     * The centerY is the default Y position of the player circle.
+     */
+    private final int centerY = 425; // Default Y position
+
+    /**
+     * The playerCenterX is the X position of the player circle.
+     */
+    private double playerCenterX = 500.0; // Default X is center
+    /**
+     * The bots is the number of bots in the game.
+     */
+    private final int botsMultiplier = 5;
+
+    /**
+     * Move the wordMaster (The letters) to a chosen location. Resets to original
      * posistion after animation.
-     * 
+     *
      * @param targetX - X position of target
      * @param targetY - Y position of target
      * @param speed   - Speed of animation in milliseconds
      */
-    public void WordMasterMoveTo(double targetX, double targetY, int speed) {
+    public void wordMasterMoveTo(final double targetX, final double targetY, final int speed) {
         TranslateTransition translate = new TranslateTransition();
-        translate.setNode(WordMaster);
+        translate.setNode(wordMaster);
         translate.setDuration(Duration.millis(speed));
-        translate.setByY(targetY - WordMaster.getLayoutY() - 30); // -30 so the reference point is the center
-        translate.setByX(targetX - WordMaster.getLayoutX() - 30);
+        translate.setByY(targetY - wordMaster.getLayoutY() - layoutCenter); // -30 so the reference point is the center
+        translate.setByX(targetX - wordMaster.getLayoutX() - layoutCenter);
         translate.play();
         translate.setOnFinished((event) -> {
-            WordMaster.setLayoutX(470); // Reset layout to defualt values
-            WordMaster.setLayoutY(70);
-            WordMaster.setTranslateX(0); // Move Wordmaster to default value
-            WordMaster.setTranslateY(0);
+            wordMaster.setLayoutX(layoutX); // Reset layout to defualt values
+            wordMaster.setLayoutY(layoutY);
+            wordMaster.setTranslateX(0); // Move wordMaster to default value
+            wordMaster.setTranslateY(0);
             translate.stop();
         });
 
     }
 
     /**
-     * Create players and draw them on the fxml
-     * 
+     * Create players and draw them on the fxml.
+     *
      * @param haveBots - If true add 1-4 bots
-     * 
      * @throws FileNotFoundException If player pictures is not found
      */
-    public void createPlayers(boolean haveBots) throws FileNotFoundException {
+    public void createPlayers(final boolean haveBots) throws FileNotFoundException {
         // TODO read from active player JSON
-        int radius = 30;
-        int centerY = 425; // Default Y position
-        double playerCenterX = 500; // Default X is center
 
         if (haveBots) {
-            double NumOfBots = Math.floor(Math.random() * 5) + 2;// minimum of 1 bot
-            double centerX = window.getPrefWidth() / (NumOfBots + 1);
-            for (int j = 1; j < (NumOfBots + 1); j++) {
-                if (j != (int) Math.ceil(NumOfBots / 2)) {
+            double numOfBots = Math.floor(Math.random() * botsMultiplier) + 2; // minimum of 1 bot
+            double centerX = window.getPrefWidth() / (numOfBots + 1);
+            for (int j = 1; j < (numOfBots + 1); j++) {
+                if (j != (int) Math.ceil(numOfBots / 2)) {
                     players.add(new Circle(centerX * j, centerY, radius, Color.BLACK));
                 } else {
                     playerCenterX = centerX * j;
@@ -101,23 +150,25 @@ public class GamePageController implements Initializable {
     }
 
     /**
-     * WordMaster checks if player written word is correct. If right add 1 point
+     * wordMaster checks if player written word is correct. If right add 1 point
+     * else shake wordMaster.
+     *
+     * @param ke - KeyEvent
      */
-    public void checkWrittenWord(KeyEvent ke) {
+    public void checkWrittenWord(final KeyEvent ke) {
         // TODO green color on right letters and red on wrong
 
         if (ke.getCode().equals(KeyCode.ENTER)) { // If pressed Enter, then check word
-            String playerGuess = PlayerInputField.getText();
+            String playerGuess = playerInputField.getText();
             if (user.checkValidWord(substring, playerGuess)) {
                 // TODO - FileWriter add points
                 FileIO.incrementHighScore();
                 int Points = FileIO.getHighScore();
                 // int Points = Integer.parseInt(points.getText()) + 1;
                 points.setText(String.valueOf(Points));
-                rndWordMasterLetters();
-            } else {
-                // TODO - Animate shake effect on wrong answer
+                rndwordMasterLetters();
             }
+            // TODO - Animate shake effect on wrong answer
 
         }
 
@@ -127,21 +178,21 @@ public class GamePageController implements Initializable {
      * Displays a random set of letters from the category answers.
      * The length of the letters is either 2 or 3.
      */
-    public void rndWordMasterLetters() {
+    public void rndwordMasterLetters() {
         String string = user.getRandomWord();
         substring = GameLogic.getRandomSubstring(string);
         letters.setText(substring);
     }
 
     @Override // Runs on start of the application
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(final URL location, final ResourceBundle resources) {
         try {
             points.setText(String.valueOf(FileIO.getHighScore()));
             user = new GameLogic("guest");
             user.setCategory("default_category1");
-            rndWordMasterLetters();
+            rndwordMasterLetters();
             createPlayers(true);
-            PlayerInputField.requestFocus();
+            playerInputField.requestFocus();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
