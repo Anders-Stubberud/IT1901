9cf05ed08ff5991198de60e1e135c90de3fcf208
andervs <andervs@stud.ntnu.no>
2023-09-20 16:38:35 +0200

    fix bug

--- ui/src/main/java/ui/GamePageController.java
+++ ui/src/main/java/ui/GamePageController.java
@@ -181,6 +181,7 @@ public final class GamePageController implements Initializable {
      */
     public void rndwordMasterLetters() {
         String string = user.getRandomWord();
+        System.out.println(string);
         substring = GameLogic.getRandomSubstring(string);
         letters.setText(substring);
     }
