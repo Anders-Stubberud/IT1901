6aa8578270288ca8654a9489d70a3e205bb7309d
madsab <mads.barnes@hotmail.com>
2023-09-20 09:59:10 +0200

    quailty fix - checkstyle

--- core/src/main/java/core/CategoryLogic.java
+++ core/src/main/java/core/CategoryLogic.java
@@ -1,66 +1,81 @@
 package core;
 
-import java.io.File;
 import java.util.Collection;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class CategoryLogic 
-{
+/**
+ * A class containing the logic of getting and setting categories.
+ */
+public final class CategoryLogic {
 
-    private String username;
+    /**
+     * Username of current player.
+     */
+    private String currentUser;
+    /**
+     * A collection of all default categories as strings.
+     */
     private Collection<String> availableDefaultCategories;
+    /**
+     * A collection of all custom categories as strings.
+     */
     private Collection<String> availableCustomCategories;
 
     /**
-     * Initializes the logic regarding the categories. 
+     * Initializes the logic regarding the categories.
      * Delegates the task of querying the available categories to FileIO.
-     * @param username The username of the user, which is used to access the available wordlists of the user.
+     * 
+     * @param username The username of the user, which is used to access the
+     *                 available wordlists of the user.
      */
-    public CategoryLogic(String username)
-    {
-        this.username = username;
+    public CategoryLogic(final String username) {
+        this.currentUser = username;
         availableDefaultCategories = FileIO.loadDefaultCategories();
-        if (! username.equals("guest"))
-        {
+        if (!username.equals("guest")) {
             availableCustomCategories = FileIO.loadCustomCategories(username);
         }
     }
 
-    public Collection<String> getAvailableDefaultCategories()
-    {
+    /**
+     * @return Collection of default categories as strings
+     */
+    public Collection<String> getAvailableDefaultCategories() {
         return availableDefaultCategories;
     }
 
-    public Collection<String> getAvailableCustomCategories()
-    {
+    /**
+     * @return Collection of custom categories as strings
+     */
+    public Collection<String> getAvailableCustomCategories() {
         return availableCustomCategories;
     }
 
     /**
      * Combines the default and custom categories.
+     * 
      * @return A collection consisting of all default and custom categories.
      */
-    public Collection<String> getAllAvailableCategories()
-    {
-        return availableCustomCategories != null ? Stream.of(availableDefaultCategories, availableCustomCategories)
-        .flatMap(Collection::stream).collect(Collectors.toList()) : availableDefaultCategories;
+    public Collection<String> getAllAvailableCategories() {
+        return (availableCustomCategories != null
+                ? Stream.of(availableDefaultCategories, availableCustomCategories)
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList())
+                : availableDefaultCategories);
     }
 
     /**
-     * Delegates the task of acquiring the wordlist for the chosen category to FileIO.
+     * Delegates the task of acquiring the wordlist for the chosen category to
+     * FileIO.
+     * 
      * @param chosenCategory the category selected by the user.
-     * @return A WorList object, which contains the wordlists for searching and selection.
+     * @return A WorList object, which contains the wordlists for searching and
+     *         selection.
      */
-    public WordLists getWordsFromChosenCategory(String chosenCategory)
-    {
-        return FileIO.createWordlist(getAvailableDefaultCategories().contains(chosenCategory), username, chosenCategory);
-    }
-
-    public static void main(String [] args)
-    {
-        System.out.println("\n\n" + System.getProperty("user.dir") + "\n\n");
+    public WordLists getWordsFromChosenCategory(final String chosenCategory) {
+        return FileIO
+                .createWordlist(getAvailableDefaultCategories()
+                        .contains(chosenCategory), currentUser, chosenCategory);
     }
 
 }
