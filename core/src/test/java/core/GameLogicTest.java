4d45952baa1cfa7f13c52d6425e0b57b175c5149
andervs <andervs@stud.ntnu.no>
2023-09-20 14:48:59 +0200

    Correct checkstyle for GameLogicTest.java

--- core/src/test/java/core/GameLogicTest.java
+++ core/src/test/java/core/GameLogicTest.java
@@ -1,53 +1,58 @@
 package core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class GameLogicTest 
-{
+public class GameLogicTest {
 
+    /**
+     * GameLogic object for guest user.
+     */
     private GameLogic guest;
+    /**
+     * GameLogic object for registered user.
+     */
     private GameLogic registeredUser;
-    
+    /**
+     * Digit for number of tests.
+     */
+    private static final int NUMBER_OF_TESTS = 1000;
     /**
      * Sets up two instances of the Category class to be used in the tests.
      */
     @BeforeEach
-    public void setUp()
-    {
+    public void setUp() {
         guest = new GameLogic("guest");
         guest.setCategory("default_category1");
         registeredUser = new GameLogic("registeredUser");
         registeredUser.setCategory("example_category1");
     }
 
+    /**
+     * Checks that the queryed wordlist is identical to the correct wordlist.
+     */
     @Test
     @DisplayName("Check correct initialization of search and selection wordlists")
-    public void testSetUpOfWordlists()
-    {
-        Collection<String> correctWordlistForGuest = FileIO.createWordlist(true, "guest", "default_category1").getWordListForSearch();
-
+    public void testSetUpOfWordlists() {
+        Collection<String> correctWordlistForGuest = FileIO
+        .createWordlist(true, "guest", "default_category1").getWordListForSearch();
         assertTrue(correctWordlistForGuest.containsAll(guest.getWordListForSearch()));
         assertTrue(guest.getWordListForSearch().containsAll(correctWordlistForGuest));
         assertTrue(correctWordlistForGuest.containsAll(guest.getWordlistForSelection()));
-        assertTrue(guest.getWordlistForSelection().containsAll(correctWordlistForGuest));        
+        assertTrue(guest.getWordlistForSelection().containsAll(correctWordlistForGuest));
     }
 
+    /**
+     * Checks that a randomly pulled word from the selection list is contained in the search list.
+     */
     @Test
     @DisplayName("Check valid pull of random word")
-    public void testGetRandomWord()
-    {
-        for (int i=0; i<1000; i++)
-        {
+    public void testGetRandomWord() {
+        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
             String randomWordFromGuest = guest.getRandomWord();
             String randomWordFromRegisteredUser = registeredUser.getRandomWord();
             assertTrue(guest.getWordListForSearch().contains(randomWordFromGuest));
@@ -58,12 +63,14 @@ public class GameLogicTest
         assertFalse(registeredUser.getWordListForSearch().contains(wordWhichIsNotInEitherCategory));
     }
 
+    /**
+     * Checks that a randomly generated substring from a randomly
+     * pulled word is indeed recognized as a valid substring.
+     */
     @Test
     @DisplayName("Check valid substring of word")
-    public void testGetRandomSubstring()
-    {
-        for (int i=0; i<1000; i++)
-        {
+    public void testGetRandomSubstring() {
+        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
             String randomWordFromGuest = guest.getRandomWord();
             String substringFromWordFromGuest = GameLogic.getRandomSubstring(randomWordFromGuest);
 
@@ -72,10 +79,12 @@ public class GameLogicTest
         }
     }
 
+    /**
+     * Checks that valid words are recognized based on a valid substring.
+     */
     @Test
     @DisplayName("Check valid guess")
-    public void testCheckValidWord()
-    {
+    public void testCheckValidWord() {
         assertTrue(guest.checkValidWord("Et", "Ethiopia"));
         assertTrue(guest.checkValidWord("et", "Vietnam"));
         assertFalse(guest.checkValidWord("Nor", "This_contains_Nor_but_is_not_in_the_wordlist"));
