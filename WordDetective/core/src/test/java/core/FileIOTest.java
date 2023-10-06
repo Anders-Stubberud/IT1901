a05494c8481e727a02e7d79791add2a2cf73a350
madsab <mads.barnes@hotmail.com>
2023-10-06 08:56:19 +0200

    expectedSize run local function + add dat function

--- WordDetective/core/src/test/java/core/FileIOTest.java
+++ WordDetective/core/src/test/java/core/FileIOTest.java
@@ -3,6 +3,9 @@ package core;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,7 +28,7 @@ public class FileIOTest {
     public void testLoadDefaultCategories() {
         Collection<String> defaultCategories = FileIO.loadDefaultCategories();
         assertNotNull(defaultCategories);
-        int expectedSize = FileIO.getNumberOfDefaultCategories();
+        int expectedSize = getDefaultCategoryLength();
         assertEquals(expectedSize, defaultCategories.size());
     }
 
@@ -47,5 +50,30 @@ public class FileIOTest {
         FileIO.incrementHighScore();
         int updatedHighScore = FileIO.getHighScore();
         assertEquals(initialHighScore + 1, updatedHighScore);
+    }
+
+    /**
+     * Overview of how many default categories there are in
+     * resources/default_categories.json.
+     *
+     * @return Integer of how many default categories.
+     *
+     */
+    private int getDefaultCategoryLength() {
+        Path path = Paths.get("").toAbsolutePath();
+        while (!path.endsWith("gr2325")) {
+            path = path.getParent();
+        }
+
+        File defaultCategoriesDirectory = new File(
+                path.toString() + "/WordDetective/core/src/main/resources/default_categories");
+
+        if (defaultCategoriesDirectory.exists() && defaultCategoriesDirectory.isDirectory()) {
+            File[] defaultCategoriesArray = defaultCategoriesDirectory.listFiles();
+            if (defaultCategoriesArray != null) {
+                return defaultCategoriesArray.length;
+            }
+        }
+        return 0;
     }
 }
