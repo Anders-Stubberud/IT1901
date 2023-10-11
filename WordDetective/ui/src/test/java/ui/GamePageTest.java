6bcd2c4986bbe820b2a28b2f165591732a427c0e
madsab <mads.barnes@hotmail.com>
2023-10-11 11:18:52 +0200

    add: testWords and cleanInput

--- WordDetective/ui/src/test/java/ui/GamePageTest.java
+++ WordDetective/ui/src/test/java/ui/GamePageTest.java
@@ -1,9 +1,23 @@
 package ui;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import core.GameLogic;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,16 +31,24 @@ public class GamePageTest extends ApplicationTest {
      */
     private GamePageController controller;
     /**
+     * Gamelogic object.
+     */
+    private GameLogic gameMock;
+    /**
      * Root node of GamePage.
      */
     private Parent root;
+    /**
+     * List for testing.
+     */
+    private List<String> testList = new ArrayList<>(Arrays.asList("TESTWORD"));
 
     /**
      * Start application.
      */
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("App.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("GamePage.fxml"));
         root = fxmlLoader.load();
         controller = fxmlLoader.getController();
         stage.setScene(new Scene(root));
@@ -49,15 +71,74 @@ public class GamePageTest extends ApplicationTest {
      */
     private String getInput() {
         return ((TextField) root.lookup("playerInputField")).getText();
+
     }
 
     /**
-     * Write a str in input field
+     * Write a str in input field.
      *
      * @param str - The string to write
      */
     private void writeInput(final String str) {
-        ((TextField) root.lookup("playerInputField")).setText(str);
+        ((TextField) getRootNode().lookup("#innerWindow").lookup("#playerInputField")).setText(str);
     }
 
+    /**
+     * Clean inputfield.
+     */
+    private void cleanInput() {
+        ((TextField) getRootNode().lookup("#innerWindow").lookup("#playerInputField")).setText("");
+    }
+
+    /**
+     * Setup before each test. Uses mockups for future testing.
+     */
+    @BeforeEach
+    public void setUp() {
+        gameMock = mock(GameLogic.class);
+        controller.setGame(gameMock);
+        gameMock.setCategory("Testing");
+        gameMock.setWordList(testList);
+    }
+
+    /**
+     * Check if guessed words is correct or incorrect.
+     *
+     * @param word          - The word to check
+     * @param wordIsCorrect - If the word is meant to be correct
+     */
+    private void isCorrect(String word, boolean wordIsCorrect) {
+        if (wordIsCorrect) {
+            assertTrue(testList.contains(word));
+        } else {
+            assertFalse(testList.contains(word));
+        }
+
+    }
+
+    /**
+     * Test writing a word input
+     *
+     * @param word      - The word to write
+     * @param isCorrect - Is word correct
+     */
+    @ParameterizedTest
+    @MethodSource
+    public void testWriteWords(String word, boolean isCorrect) {
+        writeInput(word);
+        isCorrect(getInput(), isCorrect);
+        cleanInput();
+    }
+
+    /**
+     * Stram of arguments for writing words
+     *
+     * @return - Stream of Arguments
+     */
+    private static Stream<Arguments> testWriteWords() {
+        return Stream.of(
+                Arguments.of("TESTWORD", true),
+                Arguments.of("TESTWORD", true),
+                Arguments.of("WRONGWORD", false));
+    }
 }
