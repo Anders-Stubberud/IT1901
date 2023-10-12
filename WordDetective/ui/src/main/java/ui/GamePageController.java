fa45efd0626d34c57fc441aa481dda68914338ae
madsab <mads.barnes@hotmail.com>
2023-10-12 12:07:38 +0200

    fix: Show category + show player

--- WordDetective/ui/src/main/java/ui/GamePageController.java
+++ WordDetective/ui/src/main/java/ui/GamePageController.java
@@ -1,5 +1,7 @@
 package ui;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +16,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -100,21 +104,20 @@ public final class GamePageController implements Initializable {
      */
     private final int layoutCenter = 30;
 
-    // might be used in later release
-    // /**
-    // * The radius is the radius of the player circle.
-    // */
-    // private final int radius = 30;
+    /**
+     * The radius is the radius of the player circle.
+     */
+    private final int radius = 30;
 
-    // /**
-    // * The centerY is the default Y position of the player circle.
-    // */
-    // private final int centerY = 425; // Default Y position
+    /**
+     * The centerY is the default Y position of the player circle.
+     */
+    private final int centerY = 425; // Default Y position
 
-    // /**
-    // * The playerCenterX is the X position of the player circle.
-    // */
-    // private final double pageCenter = 500.0; // Default X is center
+    /**
+     * The playerCenterX is the X position of the player circle.
+     */
+    private final double centerX = 500.0; // Default X is center
     // /**
     // * The bots is the number of bots in the game.
     // */
@@ -201,40 +204,38 @@ public final class GamePageController implements Initializable {
     }
 
     // Might be inplemented later.
-    // /**
-    // * Create players and draw them on the fxml.
-    // *
-    // * @param haveBots - If true add 1-4 bots
-    // * @throws FileNotFoundException If player pictures is not found
-    // */
-    // public void createPlayers(final boolean haveBots) throws
-    // FileNotFoundException {
-    // // TODO read from active player JSON
-    // double playerCenterX = pageCenter;
+    /**
+     * Create players and draw them on the fxml.
+     *
+     * @param haveBots - If true add 1-4 bots
+     * @throws FileNotFoundException If player pictures is not found
+     */
+    public void createPlayers(final boolean haveBots) throws FileNotFoundException {
+        // // TODO read from active player JSON
+        // double playerCenterX = pageCenter;
 
-    // if (haveBots) {
-    // double numOfBots = Math.floor(Math.random() * botsMultiplier) + 2; // minimum
-    // of 1 bot
-    // double centerX = window.getPrefWidth() / (numOfBots + 1);
-    // for (int j = 1; j < (numOfBots + 1); j++) {
-    // if (j != (int) Math.ceil(numOfBots / 2)) {
-    // players.add(new Circle(centerX * j, centerY, radius,
-    // new ImagePattern(new Image(new
-    // FileInputStream("./assets/images/Abdulbari.png")))));
-    // } else {
-    // playerCenterX = centerX * j;
-    // }
-    // }
-    // }
-    // Circle activePlayer = new Circle(playerCenterX, centerY, radius,
-    // new ImagePattern(new Image(new
-    // FileInputStream("./assets/images/Brage.png"))));
+        // if (haveBots) {
+        // double numOfBots = Math.floor(Math.random() * botsMultiplier) + 2; // minimum
+        // of 1 bot
+        // double centerX = window.getPrefWidth() / (numOfBots + 1);
+        // for (int j = 1; j < (numOfBots + 1); j++) {
+        // if (j != (int) Math.ceil(numOfBots / 2)) {
+        // players.add(new Circle(centerX * j, centerY, radius,
+        // new ImagePattern(new Image(new
+        // FileInputStream("./assets/images/Abdulbari.png")))));
+        // } else {
+        // playerCenterX = centerX * j;
+        // }
+        // }
+        // }
+        // Circle activePlayer = new Circle(playerCenterX, centerY, radius,
+        // new ImagePattern(new Image(new
+        // FileInputStream("./assets/images/Brage.png"))));
 
-    // // players.add(((int) Math.floor(players.size() / 2)), activePlayer);
-    // players.add((int) (players.size() / 2), activePlayer);
-    // window.getChildren().addAll(players);
-
-    // }
+        // // players.add(((int) Math.floor(players.size() / 2)), activePlayer);
+        // players.add((int) (players.size() / 2), activePlayer);
+        // window.getChildren().addAll(players);
+    }
 
     /**
      * game checks if player written word is correct. If right add 1 point
@@ -332,18 +333,27 @@ public final class GamePageController implements Initializable {
 
     @Override // Runs on start of the application
     public void initialize(final URL location, final ResourceBundle resources) {
-        game = new GameLogic(username);
-        game.setCategory(category);
-        rndwordMasterLetters();
-        // createPlayers(true);
-        outputField.setStyle("-fx-font: 24 arial;");
-        // Change textfield format till uppercase
-        playerInputField.setTextFormatter(new TextFormatter<>((change) -> {
-            change.setText(change.getText().toUpperCase());
-            playerInputField.setStyle("-fx-opacity: 0");
-            return change;
-        }));
-        playerInputField.requestFocus();
+        try {
+            game = new GameLogic(username);
+            game.setCategory(category);
+            rndwordMasterLetters();
+            Circle activePlayer = new Circle(centerX, centerY, radius,
+                    new ImagePattern(new Image(new FileInputStream("./assets/images/Brage.png"))));
+
+            innerWindow.getChildren().addAll(activePlayer);
+            // createPlayers(true);
+            outputField.setStyle("-fx-font: 24 arial;");
+            // Change textfield format till uppercase
+            playerInputField.setTextFormatter(new TextFormatter<>((change) -> {
+                change.setText(change.getText().toUpperCase());
+                playerInputField.setStyle("-fx-opacity: 0");
+                return change;
+            }));
+            playerInputField.requestFocus();
+            categoryDisplay.setText("Category: " + game.getChosenCategory().toUpperCase());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
