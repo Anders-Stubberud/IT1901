2c94a9d0155c931c3a27462ff4c9b31b018384d9
madsab <mads.barnes@hotmail.com>
2023-10-20 13:17:42 +0200

    update: requires types

--- WordDetective/persistence/src/main/java/module-info.java
+++ WordDetective/persistence/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module project.persistence {
     exports persistence;
 
     requires transitive com.google.gson;
+    requires transitive project.types;
 
     opens persistence to com.google.gson;
 }
