f3830fd4f865ac363ccf1b9110ec933b03e78fbf
madsab <mads.barnes@hotmail.com>
2023-10-24 12:30:12 +0200

    quality: checkstyle

--- WordDetective/persistence/src/main/java/persistence/AbstractJsonIO.java
+++ WordDetective/persistence/src/main/java/persistence/AbstractJsonIO.java
@@ -11,14 +11,14 @@ public interface AbstractJsonIO {
    *
    * @param user - The user to add
    */
-  public void addUser(final User user);
+  void addUser(User user);
 
   /**
    * Delete user from database.
    *
    * @param username - The user to delete
    */
-  public void deleteUser(String username);
+  void deleteUser(String username);
 
   /**
    * Get user as a {@link User} from database.
@@ -26,14 +26,14 @@ public interface AbstractJsonIO {
    * @param username - The user to get
    * @return - A {@link User} object
    */
-  public User getUser(String username);
+  User getUser(String username);
 
   /**
    * Update user and store new data in database.
-   *
-   * @param username - The new user object to override the old
+   * 
+   * @param user - The new user object to override the old
    */
-  public void updateUser(User user);
+  void updateUser(User user);
 
   /**
    * Get a defaultCategory as a {@link List}.
@@ -41,12 +41,12 @@ public interface AbstractJsonIO {
    * @param category - The category to get
    * @return - A {@link List} of answers from that category
    */
-  public List<String> getDefaultCategory(String category);
+  List<String> getDefaultCategory(String category);
 
   /**
    * Get all the current default categories.
    *
    * @return - A {@link HashMap} of category names and respective answers
    */
-  public HashMap<String, List<String>> getAllDefaultCategories();
+  HashMap<String, List<String>> getAllDefaultCategories();
 }
