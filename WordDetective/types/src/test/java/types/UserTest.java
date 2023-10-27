2fd3a9bb35d0759b9f77d5377b33e91a1727a303
madsab <mads.barnes@hotmail.com>
2023-10-27 21:59:48 +0200

    100% Jacoco test coverage

--- WordDetective/types/src/test/java/types/UserTest.java
+++ WordDetective/types/src/test/java/types/UserTest.java
@@ -1,23 +1,52 @@
 package types;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserTest {
   /**
    * Invalid user used for testing.
    */
-  private User invalidUser = new User("guest", "wrong");
+  private User invalidUser;
   /**
    * Invalid user used for testing.
    */
-  private User invalidUser2 = new User("no/()", "wrong");
+  private User invalidUser2;
   /**
    * Valid user used for testing.
    */
-  private User validUser = new User("Testuser", "Correct123");
+  private User validUser;
+
+  @BeforeEach
+  public void init() {
+    invalidUser = new User("guest", "wrong");
+    invalidUser2 = new User("no/()", "wrong");
+    validUser = new User("Testuser", "Correct123");
+  }
+
+  @Test
+  public void constructorTest() {
+    User guest = new User();
+    assertEquals(guest.getUsername(), "guest",
+        "Username should be guest on empty constructor not: " + guest.getUsername());
+    assertEquals(guest.getPassword(), "", "Password should be empty on guest, not:" + guest.getPassword());
+    assertEquals(guest.getHighScore(), 0, "Higscore should be 0, not:" + guest.getHighScore());
+
+    User user = new User("Username", "Password123");
+    assertEquals(user.getUsername(), "Username", "Username should be 'Username' not: " + user.getUsername());
+    assertEquals(user.getPassword(), "Password123",
+        "Password should be 'Password123' on user, not:" + user.getPassword());
+    assertEquals(user.getHighScore(), 0, "Higscore should be 0, not:" + user.getHighScore());
+  }
 
   /**
    * Test for valid and invalid usernames.
@@ -40,4 +69,33 @@ public class UserTest {
     assertFalse(invalidUser.isCorrectPassword(),
         "The username " + invalidUser.getPassword() + " should be invalid");
   }
+
+  @Test
+  public void highscoreTest() {
+    assertEquals(validUser.getHighScore(), 0, "Highscore should be 0, not:" + validUser.getHighScore());
+    validUser.setHighscore(20);
+    assertEquals(validUser.getHighScore(), 20, "Highscore should be 20, not:" + validUser.getHighScore());
+    validUser.setHighscore(50);
+    assertEquals(validUser.getHighScore(), 50, "Highscore should be 20, not:" + validUser.getHighScore());
+  }
+
+  @Test
+  public void customCategoriesTest() {
+    HashMap<String, List<String>> hashmap = new HashMap<>();
+    assertEquals(validUser.getCustomCategories(), hashmap, "Custom categories should be empty");
+
+    hashmap.put("TestCategory", Arrays.asList("Test", "Test", "Test"));
+    validUser.addCustomCategories("TestCategory", Arrays.asList("Test", "Test", "Test"));
+    assertEquals(validUser.getCustomCategories(), hashmap, "Custom categories should contain " + hashmap.toString());
+
+    hashmap.put("TestCategory2", Arrays.asList("Test", "Test", "Test"));
+    hashmap.put("TestCategory3", Arrays.asList("Test", "Test", "Test"));
+    validUser.setCustomCategories(hashmap);
+    assertEquals(validUser.getCustomCategories(), hashmap, "Custom categories should be same as hashmap");
+
+    validUser.deleteCustomCategories("TestCategory");
+    assertNull(validUser.getCustomCategories().get("TestCategory"), "TestCategory should have been deleted");
+
+  }
+
 }
