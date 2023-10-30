b9b609febe4e38b50309fae99fe9dede532337ef
madsab <mads.barnes@hotmail.com>
2023-10-30 14:04:14 +0100

    add: categoryTest, wordlistTet

--- WordDetective/core/src/test/java/core/GameTest.java
+++ WordDetective/core/src/test/java/core/GameTest.java
@@ -1,5 +1,21 @@
 package core;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import types.User;
+
 // import static org.junit.jupiter.api.Assertions.assertFalse;
 // import static org.junit.jupiter.api.Assertions.assertTrue;
 // import java.util.Collection;
@@ -12,89 +28,84 @@ package core;
 
 public class GameTest {
 
-    // /**
-    // * GameLogic object for guest user.
-    // */
-    // private Game guest;
-    // /**
-    // * GameLogic object for registered user.
-    // */
-    // private Game registeredUser;
-    // /**
-    // * Digit for number of tests.
-    // */
-    // private static final int NUMBER_OF_TESTS = 1000;
+    /**
+     * Mock object of game.
+     */
+    @Mock
+    private Game game;
 
-    // /**
-    // * Sets up two instances of the Category class to be used in the tests.
-    // */
-    // @BeforeEach
-    // public void setUp() {
-    // User testUser = new User("TestName", "TestPassword");
-    // guest = new Game();
-    // guest.setCategory("countries");
-    // registeredUser.setCategory("example category1");
-    // }
+    /**
+     * Mock of the user in game.
+     */
+    @Mock
+    private User testUser;
+    /**
+     * Digit for number of tests.
+     */
+    private static final int NUMBER_OF_TESTS = 1000;
 
-    // /**
-    // * Checks that the queryed wordlist is identical to the correct wordlist.
-    // */
-    // @Test
-    // @DisplayName("Check correct initialization of search and selection
-    // wordlists")
-    // public void testSetUpOfWordlists() {
+    /**
+     * Sets up two instances of the Category class to be used in the tests.
+     */
+    @BeforeEach
+    public void setUp() {
+        testUser = mock(User.class);
+        testUser.addCustomCategories("Custom", Arrays.asList("1", "3", "4"));
+        game = mock(new Game(testUser));
+    }
 
-    // }
+    /**
+     * Checks that getting/setting of category is correct.
+     */
+    @Test
+    @DisplayName("Check correct get/set of category")
+    public void categoryTest() {
+        assertNull(game.getChosenCategory());
+        game.setCategory("fruits");
+        assertEquals(game.getChosenCategory(), "fruits");
+        assertThrows(IllegalArgumentException.class, () -> game.setCategory("NonExisitingCategory"),
+                "Should not be able to set category to a category that does not exist");
+        assertEquals(game.getChosenCategory(), "fruits");
 
-    // /**
-    // * Checks that a randomly pulled word from the selection list is contained in
-    // * the search list.
-    // */
-    // @Test
-    // @DisplayName("Check valid pull of random word")
-    // public void testGetRandomWord() {
-    // for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-    // String randomWordFromGuest = guest.getRandomWord();
-    // String randomWordFromRegisteredUser = registeredUser.getRandomWord();
-    // // assertTrue(guest.getWordListForSearch().contains(randomWordFromGuest));
-    // //
-    // assertTrue(registeredUser.getWordListForSearch().contains(randomWordFromRegisteredUser));
-    // }
-    // String wordWhichIsNotInEitherCategory = "thisWordIsNotInEitherCategory";
-    // // assertFalse(guest..contains(wordWhichIsNotInEitherCategory));
-    // //
-    // assertFalse(registeredUser.getWordListForSearch().contains(wordWhichIsNotInEitherCategory));
-    // }
+    }
 
-    // /**
-    // * Checks that a randomly generated substring from a randomly
-    // * pulled word is indeed recognized as a valid substring.
-    // */
-    // @Test
-    // @DisplayName("Check valid substring of word")
-    // public void testGetRandomSubstring() {
-    // for (int i = 0; i < NUMBER_OF_TESTS; i++) {
-    // String randomWordFromGuest = guest.getRandomWord();
-    // // String substringFromWordFromGuest =
-    // Game.getRandomSubstring(randomWordFromGuest);
+    /**
+     * Check that quering and setting wordlist is correct.
+     */
+    @Test
+    @DisplayName("Check correct get/set of wordlist")
+    public void wordListTest() {
+        
+        assertTrue(game.getWordList().isEmpty());
+        game.setCategory("fruits");
+        as
 
-    // // assertTrue(randomWordFromGuest.matches(".*" + substringFromWordFromGuest +
-    // ".*"));
-    // // assertFalse(randomWordFromGuest.matches(".*" +
-    // "thisSubstringIsNotValidAnywhere" + ".*"));
-    // }
-    // }
+    }
 
-    // /**
-    // * Checks that valid words are recognized based on a valid substring.
-    // */
-    // @Test
-    // @DisplayName("Check valid guess")
-    // public void testCheckValidWord() {
-    // assertTrue(guest.checkValidWord("ET", "ETHIOPIA"));
-    // assertTrue(guest.checkValidWord("ET", "VIETNAM"));
-    // assertFalse(guest.checkValidWord("NOR",
-    // "THIS_CONTAINS_NOR_BUT_IS_NOT_IN_WORDLIST"));
-    // }
+    /**
+     * Checks that a randomly pulled word from the selection list is contained in
+     * the search list.
+     */
+    @Test
+    @DisplayName("Check valid pull of random word")
+    public void testGetRandomWord() {
+        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
+
+            // assertTrue(guest.getWordListForSearch().contains(randomWordFromGuest));
+            //
+
+        }
+    }
+
+    /**
+     * Checks that a randomly generated substring from a randomly
+     * pulled word is indeed recognized as a valid substring.
+     */
+    @Test
+    @DisplayName("Check valid substring of word")
+    public void testGetRandomSubstring() {
+        for (int i = 0; i < NUMBER_OF_TESTS; i++) {
+        }
+    }
 
 }
