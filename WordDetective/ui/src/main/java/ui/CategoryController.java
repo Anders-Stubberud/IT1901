464ca10c9b4f088bb3115b6455f285903b0d18be
madsab <mads.barnes@hotmail.com>
2023-11-14 15:37:42 +0100

    checkstyle:check

--- WordDetective/ui/src/main/java/ui/CategoryController.java
+++ WordDetective/ui/src/main/java/ui/CategoryController.java
@@ -2,8 +2,6 @@ package ui;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
