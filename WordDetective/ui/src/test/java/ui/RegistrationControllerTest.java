c64cce5a40bbb6b9368293de0a2074e36939487e
madsab <mads.barnes@hotmail.com>
2023-11-14 22:00:33 +0100

    add Tests for testing errors and success

--- WordDetective/ui/src/test/java/ui/RegistrationControllerTest.java
+++ WordDetective/ui/src/test/java/ui/RegistrationControllerTest.java
@@ -1,17 +1,27 @@
 package ui;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 
-// import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.testfx.framework.junit5.ApplicationTest;
 // import javafx.scene.control.Label;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 // import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import types.RegistrationStatus;
 
 public class RegistrationControllerTest extends ApplicationTest {
 
@@ -21,36 +31,37 @@ public class RegistrationControllerTest extends ApplicationTest {
   private Parent root;
 
   /**
-   * The loader used to instantiate.
+   * The label used to display error.
    */
-  private FXMLLoader fxmlLoader;
+  private Label errorDisplay;
 
   /**
-   * Properties to get the gitlab CI pipeline to run headless for Integration
-   * tests.
+   * The textfield used to provide username or password.
    */
-  @BeforeClass
-  public static void headless() {
-    System.setProperty("prism.verbose", "true");
-    System.setProperty("java.awt.headless", "true");
-    System.setProperty("testfx.robot", "glass");
-    System.setProperty("testfx.headless", "true");
-    System.setProperty("glass.platform", "Monocle");
-    System.setProperty("monocle.platform", "Headless");
-    System.setProperty("prism.order", "sw");
-    System.setProperty("prism.text", "t2k");
-    System.setProperty("testfx.setup.timeout", "2500");
-  }
+  private TextField usernameField, passwordField;
 
   /**
    * Instantiates the stage.
    */
   @Override
   public void start(final Stage stage) throws IOException {
-    fxmlLoader = new FXMLLoader(this.getClass().getResource("Registration.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("Registration.fxml"));
     root = fxmlLoader.load();
     stage.setScene(new Scene(root));
     stage.show();
+    errorDisplay = (Label) root.lookup("#errorDisplay");
+    usernameField = (TextField) root.lookup("#newUsername");
+    passwordField = (TextField) root.lookup("#newPassword");
+  }
+
+  /**
+   * Tests that a user can not be created if the fields are blank.
+   */
+  @Test
+  public void testBlankFields() {
+    clickOn("#signUp", MouseButton.PRIMARY);
+    assertTrue(errorDisplay.getText().contains("blank"),
+        "The error display should say something about blank fields, but was:" + errorDisplay.getText());
   }
 
   /**
@@ -58,32 +69,82 @@ public class RegistrationControllerTest extends ApplicationTest {
    */
   @Test
   public void testUsernameTaken() {
-    // clickOn("#newUsername", MouseButton.PRIMARY);
-    // write("occupiedUser");
-    // clickOn("#newPassword", MouseButton.PRIMARY);
-    // write("occupied");
-    // clickOn("#signUp", MouseButton.PRIMARY);
-    // int opacity = (int) ((Label) root.lookup("#usernameTaken")).getOpacity();
-    // Assert.assertEquals(
-    // "The opacity of the 'occupied username' label should be 1",
-    // 1, opacity);
+    write("TestUser", usernameField);
+    write("Password", passwordField);
+    clickOn("#signUp", MouseButton.PRIMARY);
+    assertTrue(errorDisplay.getText().contains("already taken"),
+        "The error display should say something about the username being taken, but was:"
+            + errorDisplay.getText());
   }
 
-  // /**
-  // * Tests that a user with valid username can be created.
-  // */
-  // @Test
-  // public void testCreateNewUser() {
-  // String username = "availableUser";
-  // Assert.assertFalse(UserIO.getAllUsernames().contains(username));
-  // clickOn("#newUsername", MouseButton.PRIMARY);
-  // write(username);
-  // clickOn("#newPassword", MouseButton.PRIMARY);
-  // write("password123");
-  // clickOn("#signUp", MouseButton.PRIMARY);
-  // Assert.assertTrue(UserIO.getAllUsernames().contains(username));
-  // File file = new File(UserIO.getPath() + "/" + username);
-  // UserIO.deleteUser(file);
-  // }
+  /**
+   * Tests that a user can not be created the username doesn't match the criteria.
+   */
+  @Test
+  public void testWrongUsername() {
+    write("T", usernameField);
+    write("Pasword123", passwordField);
+    clickOn("#signUp", MouseButton.PRIMARY);
+    assertTrue(errorDisplay.getText().contains("username needs"),
+        "The error display should say something about what is required in a username, but was:"
+            + errorDisplay.getText());
+  }
+
+  /**
+   * Tests that a user can not be created the password doesn't match the criteria.
+   * Make sure user does not exist in database.
+   */
+  @Test
+  public void testWrongPassword() {
+    write("ValidUserThatShouldntBeInDatabase", usernameField);
+    write("P", passwordField);
+    clickOn("#signUp", MouseButton.PRIMARY);
+    assertTrue(errorDisplay.getText().contains("password needs"),
+        "The error display should say something about what is required in a password, but was:"
+            + errorDisplay.getText());
+
+  }
+
+  /**
+   * Tests that a user with valid username and password can be created.
+   * Mocks the API call
+   */
+  @Test
+  @ExtendWith(MockitoExtension.class)
+  @PrepareForTest(ApiConfig.class)
+  public void testCreateNewUser() {
+    write("ValidUserThatShouldntBeInDatabase", usernameField);
+    write("Password123", passwordField);
+
+    PowerMockito.mockStatic(ApiConfig.class);
+    try {
+      when(ApiConfig.registrationResult(usernameField.getText(),
+          passwordField.getText()))
+          .thenReturn(RegistrationStatus.SUCCESS);
+
+      RegistrationController registrationController = new RegistrationController();
+
+      registrationController.fireSignUp();
+      PowerMockito.verifyStatic(ApiConfig.class);
+      ApiConfig.registrationResult(usernameField.getText(),
+          passwordField.getText());
+
+      assertEquals(0, errorDisplay.getOpacity(), "No error should be displayed");
+
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * write a string to a textfield instant.
+   *
+   * @param string    - the string to write
+   * @param textfield - the textfield to write to
+   *
+   */
+  private void write(final String string, final TextField textfield) {
+    textfield.setText(string);
+  }
 
 }
