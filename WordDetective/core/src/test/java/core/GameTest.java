860829287664cf8ed2105eaae3db0153ac4e22f3
dagkandPC <dagkand@ntnu.no>
2023-11-14 18:32:36 +0100

    remove comments

--- WordDetective/core/src/test/java/core/GameTest.java
+++ WordDetective/core/src/test/java/core/GameTest.java
@@ -22,11 +22,11 @@ public class GameTest {
     private Game game;
 
     /**
-     * User in game.
+     * User in the game.
      */
     private User testUser;
     /**
-     * Digit for number of tests.
+     * Digit for the number of tests.
      */
     private static final int NUMBER_OF_TESTS = 10;
     /**
@@ -34,7 +34,7 @@ public class GameTest {
      */
     private List<String> singleTestList = Arrays.asList("Test");
     /**
-     * Wordlist used for testing with mulitple strings.
+     * Wordlist used for testing with multiple strings.
      */
     private List<String> multipleTestList = Arrays.asList("Test1", "Test2", "Test3");
 
@@ -59,10 +59,10 @@ public class GameTest {
     }
 
     /**
-     * Check that quering and setting wordlist is correct.
+     * Check that querying and setting the word list is correct.
      */
     @Test
-    @DisplayName("Check correct get/set of wordlist")
+    @DisplayName("Check correct get/set of word list")
     public void testWordList() {
         String fruitWord = "Apple";
         assertNull(game.getWordList());
@@ -70,14 +70,13 @@ public class GameTest {
         assertEquals(game.getWordList(), singleTestList);
         game.setCategory("fruits");
         assertTrue(game.getWordList().contains(fruitWord.toUpperCase()),
-                "The word " + fruitWord + " should be in wordlist. Your list:" + game.getWordList());
+                "The word " + fruitWord + " should be in the word list. Your list:" + game.getWordList());
         assertFalse(game.getWordList().contains(singleTestList.get(0)),
-                " The wordlist should not contain the word " + singleTestList.get(0) + " anymore");
+                " The word list should not contain the word " + singleTestList.get(0) + " anymore");
     }
 
     /**
-     * Checks that a randomly generated substring from a randomly.
-     * pulled word is indeed recognized as a valid substring.
+     * Checks that a randomly generated substring from a randomly pulled word is indeed recognized as a valid substring.
      */
     @Test
     @DisplayName("Check valid substring of word")
@@ -88,28 +87,26 @@ public class GameTest {
         }
     }
 
-    // /**
-    // * Test checking valid words.
-    // */
-    // @Test
-    // @DisplayName("Check that guesses are valid")
-    // public void testCheckValidWord() {
-    // game.setWordList(multipleTestList);
-    // assertTrue(game.checkValidWord("s", "Test"));
-    // assertTrue(game.checkValidWord("es", "Test"));
-    // assertTrue(game.checkValidWord("st2", "Test2"));
-    // assertFalse(game.checkValidWord("Tes", "Test4"));
-    // assertFalse(game.checkValidWord("2", "Test"));
-    // assertFalse(game.checkValidWord("es", "Test4"));
-    // }
+    /**
+     * Test checking valid words.
+     */
+    @Test
+    @DisplayName("Check that guesses are valid")
+    public void testCheckValidWord() {
+        game.setWordList(multipleTestList);
+        assertTrue(game.checkValidWord("s", "Test"));
+        assertTrue(game.checkValidWord("es", "Test"));
+        assertTrue(game.checkValidWord("st2", "Test2"));
+        assertFalse(game.checkValidWord("Tes", "Test4"));
+        assertFalse(game.checkValidWord("2", "Test"));
+        assertFalse(game.checkValidWord("es", "Test4"));
+    }
 
-    // @Test
-    // @DisplayName("Check setting of highscore")
-    // public void testHighscore() {
-    // assertEquals(0, game.getPlayerHighscore(), "Highscore should be 0 on start");
-    // game.savePlayerHighscore(100);
-    // assertEquals(100, game.getPlayerHighscore(),
-    // "Highscore should be 100, but was " + game.getPlayerHighscore());
-    // }
-
+    @Test
+    @DisplayName("Check setting of high score")
+    public void testHighscore() {
+        assertEquals(0, game.getPlayerHighscore(), "High score should be 0 on start");
+        game.savePlayerHighscore(100);
+        assertEquals(100, game.getPlayerHighscore(), "High score should be 100, but was " + game.getPlayerHighscore());
+    }
 }
