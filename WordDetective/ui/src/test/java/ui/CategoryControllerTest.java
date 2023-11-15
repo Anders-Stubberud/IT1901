fea99110f0a02574ad9b4286e5109ea81bd547f3
madsab <mads.barnes@hotmail.com>
2023-11-15 16:38:59 +0100

    removed headless

--- WordDetective/ui/src/test/java/ui/CategoryControllerTest.java
+++ WordDetective/ui/src/test/java/ui/CategoryControllerTest.java
@@ -14,23 +14,6 @@ public class CategoryControllerTest extends ApplicationTest {
      */
     private Parent root;
 
-    /**
-     * Properties t0 get the gitlab CI pipeline to run headless for Integration
-     * tests.
-     */
-    @BeforeClass
-    public static void headless() {
-        System.setProperty("prism.verbose", "true");
-        System.setProperty("java.awt.headless", "true");
-        System.setProperty("testfx.robot", "glass");
-        System.setProperty("testfx.headless", "true");
-        System.setProperty("glass.platform", "Monocle");
-        System.setProperty("monocle.platform", "Headless");
-        System.setProperty("prism.order", "sw");
-        System.setProperty("prism.text", "t2k");
-        System.setProperty("testfx.setup.timeout", "2500");
-    }
-
     // /**
     // * Instantiates the stage.
     // */
