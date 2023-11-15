975df2b0d3bc9780628e79980d91c6f687b935e6
andervs <andervs@stud.ntnu.no>
2023-11-15 11:05:15 +0100

    change: checkstyle.

--- WordDetective/types/src/test/java/types/LoginStatusTest.java
+++ WordDetective/types/src/test/java/types/LoginStatusTest.java
@@ -5,25 +5,36 @@ import org.junit.jupiter.api.Test;
 
 public class LoginStatusTest {
 
+  /**
+   * Test correct value of "SUCCESS".
+   */
   @Test
   public void testSuccess() {
     assertEquals(LoginStatus.SUCCESS, LoginStatus.valueOf("SUCCESS"));
   }
 
+  /**
+   * Test correct value of "USERNAME_DOES_NOT_EXIST".
+   */
   @Test
   public void testUsernameDoesNotExist() {
     assertEquals(LoginStatus.USERNAME_DOES_NOT_EXIST, LoginStatus.valueOf("USERNAME_DOES_NOT_EXIST"));
   }
 
+  /**
+   * Test correct value of "INCORRECT_PASSWORD".
+   */
   @Test
   public void testIncorrectPassword() {
     assertEquals(LoginStatus.INCORRECT_PASSWORD, LoginStatus.valueOf("INCORRECT_PASSWORD"));
   }
 
+  /**
+   * Test correct value of "READ_ERROR".
+   */
   @Test
   public void testReadError() {
     assertEquals(LoginStatus.READ_ERROR, LoginStatus.valueOf("READ_ERROR"));
   }
 
-  // Add more tests as needed
 }
