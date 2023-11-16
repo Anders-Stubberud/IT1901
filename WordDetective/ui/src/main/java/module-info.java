1285f08150de151592613e0373a2cc925e1cabc2
madsab <mads.barnes@hotmail.com>
2023-11-16 10:12:55 +0100

    removed dependency core, persistence. Add gson

--- WordDetective/ui/src/main/java/module-info.java
+++ WordDetective/ui/src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module project.ui {
-    requires project.core;
-    requires project.persistence;
+
     requires project.types;
     requires javafx.controls;
     requires javafx.media;
     requires javafx.fxml;
     requires java.net.http;
+    requires com.google.gson;
 
     opens ui to javafx.graphics, javafx.fxml, javafx.media;
 }
