9078eb2b439289a77e79561158869aaadbf50057
barothei <barothei@ntnu.no>
2023-11-16 14:27:23 +0100

    Javadoc checkstyle

--- WordDetective/ui/src/main/java/ui/GamePageController.java
+++ WordDetective/ui/src/main/java/ui/GamePageController.java
@@ -488,6 +488,11 @@ public final class GamePageController extends AbstractController implements Init
 
     }
 
+    /**
+     * This formats categories that appear in game page
+     * @param input
+     * @return
+     */
     public String formatString(final String input) {
         String[] words = input.split(" ");
         StringBuilder formattedString = new StringBuilder();
