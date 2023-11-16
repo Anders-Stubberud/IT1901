730cc30bd9075987c84500ba7e458777a81ac27b
dagkand <dagkand@ntnu.no>
2023-11-16 12:13:25 +0100

    add runtime

--- WordDetective/ui/src/main/java/ui/GamePageController.java
+++ WordDetective/ui/src/main/java/ui/GamePageController.java
@@ -494,7 +494,7 @@ public final class GamePageController extends AbstractController implements Init
         try {
             imageGame.setImage(
                     new Image(new FileInputStream(
-                            Paths.get("assets").toAbsolutePath() + "/images/gamepagenew.png")));
+                            Paths.get("runtime/assets").toAbsolutePath() + "/images/gamepagenew.png")));
             api.newGame(username, currentCategory);
             rndwordMasterLetters();
             playerCircle = new Circle(centerX, centerY, radius,
